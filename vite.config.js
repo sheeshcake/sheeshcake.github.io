60c0787342bc0b9388cd3950345d4a0ee75577ab
Wendale Dy <wendalefranz.dy@gmail.com>
2024-08-30 20:43:00 +0800

    UPDATE - fixes on setup

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sheeshcake.github.io",
+  base: "/",
   resolve: {
       alias: {
           process: "process/browser",
